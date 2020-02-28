99520a28304a28d8da3f576b0a9f964d456ff994
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-28 11:41:47 -0500

    add in spotify authorization

--- my-spotify/src/App.js
+++ my-spotify/src/App.js
@@ -1,11 +1,63 @@
 import React from 'react';
+import Spotify from 'spotify-web-api-js'; 
+import Home from './Components/Home/Home';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const spotifyWebApi = new Spotify();
+
+class App extends React.Component {
+  constructor() {
+    super()
+    const {access_token} = this.getHashParams();
+    this.state = {
+      loggedIn: access_token ? true : false,
+      nowPlaying: {
+        name: 'Not Checked',
+        img: ""
+      }
+    }
+    if(access_token) {
+      spotifyWebApi.setAccessToken(access_token);
+    }
+  }
+  
+  getHashParams = () => {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying = async () => {
+    let res = await spotifyWebApi.getMyCurrentPlaybackState();
+    // debugger;
+    this.setState({
+      nowPlaying: {
+        name: res.item.name,
+        img: res.item.album.images[0].url
+      }
+    })
+  }
+  
+  render = () => {
+    let {loggedIn} = this.state;
+    return (
+      <div className="App">
+        <a href="http://localhost:8888">
+          <button>Login With Spotify</button>
+        </a>
+          {/* <Home value={access_token}/> */}
+          <div> Now Playing: {this.state.nowPlaying.name} </div>
+          <img alt="album" src={ this.state.nowPlaying.img } style={{width: 100}}/>
+          <button onClick={() => this.getNowPlaying()}>Check Now Playing</button>
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- my-spotify/src/index.js
+++ my-spotify/src/index.js
@@ -3,13 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import SpotifyApiContext from 'react-spotify-api';
 
-ReactDOM.render(
-    <SpotifyApiContext.Provider value={"5faa67c1df8d44f5ba10b001e724ad6a"}>
-        <App />
-    </SpotifyApiContext.Provider>, 
-document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
