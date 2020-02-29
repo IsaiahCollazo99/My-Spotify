5183b47b5532d59099b8311ce58da653b5fc5dfb
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-29 17:33:36 -0500

    change TopTracks component from class to hooks

--- my-spotify/src/Components/Home/TopTracks.js
+++ my-spotify/src/Components/Home/TopTracks.js
@@ -1,36 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Track from './../General/Track';
 
-class TopTracks extends React.Component {
-    state = {
-        topTracks: []
+const TopTracks = ({spotifyWebApi}) => {
+    const [topTracks, setTopTracks] = useState([]);
+
+    const getTopTracks = async () => {
+        let res = await spotifyWebApi.getMyTopTracks();
+        setTopTracks(res.items);
     }
 
-    componentDidMount = () => {
-        let {spotifyWebApi} = this.props;
-        spotifyWebApi.getMyTopTracks().then(res => {
-            this.setState(prevState => ({topTracks: res.items}));
-        })
-    }
+    useEffect(() => {
+        getTopTracks();
+    }, [])
 
-    render = () => {
-        let {topTracks} = this.state;
-        let tracksToShow;
-        if(topTracks.length) {
-            tracksToShow = topTracks.map((track, i) => {
-                if(i > 5) return null;
-                return <Track name={track.name} img={track.album.images[0].url} artist={track.artists[0].name} key={track.id}/>
-            })
-        }
-        return (
-            <div>
-                <h2>Your Top Tracks</h2>
-                <div id="topTracks">
-                    {tracksToShow}
-                </div>
+    let tracksToShow = topTracks.map((track, i) => {
+        if(i > 5) return null;
+        return <Track name={track.name} img={track.album.images[0].url} artist={track.artists[0].name} key={track.id}/>
+    });
+    
+    return (
+        <div>
+            <h2>Your Top Tracks</h2>
+            <div id="topTracks">
+                {tracksToShow}
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default TopTracks;
