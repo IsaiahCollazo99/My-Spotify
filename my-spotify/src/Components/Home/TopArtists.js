327993c0297cb8e1a970e55e4f412333173e0774
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-29 17:30:43 -0500

    change TopArtists COmponent from class to hooks

--- my-spotify/src/Components/Home/TopArtists.js
+++ my-spotify/src/Components/Home/TopArtists.js
@@ -1,36 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Artist from './../General/Artist';
 
-class TopArtists extends React.Component {
-    state = {
-        topArtists: []
+const TopArtists = ({spotifyWebApi}) => {
+    const [topArtists, setTopArtists] = useState([]);
+
+    const getTopArtists = async () => {
+        let res = await spotifyWebApi.getMyTopArtists();
+        setTopArtists(res.items);
     }
 
-    componentDidMount = () => {
-        let {spotifyWebApi} = this.props;
-        spotifyWebApi.getMyTopArtists().then(res => {
-            this.setState(prevState => ({topArtists: res.items}));
-        })
-    }
+    useEffect(() => {
+        getTopArtists();
+    }, [])
 
-    render = () => {
-        let {topArtists} = this.state;
-        let artistsToShow;
-        if(topArtists.length) {
-            artistsToShow = topArtists.map((artist, i) => {
-                if(i > 5) return null;
-                return <Artist name={artist.name} img={artist.images[0].url} key={artist.id}/>
-            })
-        }
-        return (
-            <div>
-                <h2>Your Top Artists</h2>
-                <div id="topArtists">
-                    {artistsToShow}
-                </div>
+    let artistsToShow = topArtists.map((artist, i) => {
+        if(i > 5) return null;
+        return <Artist name={artist.name} img={artist.images[0].url} key={artist.id}/>
+    });
+    
+    return (
+        <div>
+            <h2>Your Top Artists</h2>
+            <div id="topArtists">
+                {artistsToShow}
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default TopArtists;
