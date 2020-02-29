e5050f72ca4738e8baa46bc4140744ea96ff2878
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-29 17:45:26 -0500

    change LibraryPlaylists Component from class to hooks

--- my-spotify/src/Components/Library/LibraryPlaylists.js
+++ my-spotify/src/Components/Library/LibraryPlaylists.js
@@ -1,40 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Playlist from '../General/Playlist';
 import './../../css/LibraryPlaylists.css';
 
-class LibraryPlaylists extends React.Component {
+const LibraryPlaylists = ({spotifyWebApi}) => {
+    const [playlists, setPlaylists] = useState([]);
 
-    state = {
-        playlists: []
-    }
-
-    async componentDidMount() {
-        let res = await this.getPlaylists();
-        this.setState({playlists: res});
-    }
-
-    getPlaylists = async () => {
-        const {spotifyWebApi} = this.props;
-        let output = [];
+    const getPlaylists = async () => {
         let res = await spotifyWebApi.getUserPlaylists({limit: 50});
-        return res.items;
+        setPlaylists(res.items)
     }
 
-    render() {
-        let {playlists} = this.state;
-        let playlistsDisplay = playlists.map(playlist => {
-            return <Playlist type={"library"} img={playlist.images[0].url} name={playlist.name} owner={playlist.owner.display_name} desc={playlist.description} key={playlist.id}/>
-        })
+    useEffect(() => {
+        getPlaylists();
+    }, [])
 
-        playlistsDisplay.unshift(
-            <Playlist type={"library"} img={"https://t.scdn.co/images/3099b3803ad9496896c43f22fe9be8c4.png"} name={"Liked Songs"} key={"likedSongs"} />
-        )
-        return (
-            <div id="libraryPlaylists">
-                {playlistsDisplay}
-            </div>
-        )
-    }
+    let playlistsDisplay = playlists.map(playlist => {
+        return <Playlist type={"library"} img={playlist.images[0].url} name={playlist.name} owner={playlist.owner.display_name} desc={playlist.description} key={playlist.id}/>
+    })
+
+    playlistsDisplay.unshift(
+        <Playlist type={"library"} img={"https://t.scdn.co/images/3099b3803ad9496896c43f22fe9be8c4.png"} name={"Liked Songs"} key={"likedSongs"} />
+    )
+    
+    return (
+         <div id="libraryPlaylists">
+            {playlistsDisplay}
+        </div>
+    )
 }
 
 export default LibraryPlaylists
