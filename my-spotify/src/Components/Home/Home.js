ef7273336a3e67083b819f2a15b73555edb82616
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-28 19:10:52 -0500

    start NewReleases

--- my-spotify/src/Components/Home/Home.js
+++ my-spotify/src/Components/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './../Navbar/Navbar';
 import RecentlyPlayed from './RecentlyPlayed';
 import './../../css/Home.css';
+import NewReleases from './NewReleases';
 
 const Home = ({token, spotifyWebApi}) => {
     return (
         <div id="homeDiv">
             <Navbar />
             <RecentlyPlayed token={token}  spotifyWebApi={spotifyWebApi}/>
+            <NewReleases token={token}  spotifyWebApi={spotifyWebApi} />
         </div>
     )
 }
