58ee59cbd0195f4251a574f245b6f309672e95b2
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-08-05 01:59:10 -0400

    Updates

---
+++ precache-manifest.363689ed09b8a71986987b602cfa1add.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16d56ae645dcbcabfd96e9f836b8f22",
+    "url": "/My-Spotify/index.html"
+  },
+  {
+    "revision": "dba25a87cd8be612afc7",
+    "url": "/My-Spotify/static/css/main.2d07b8e1.chunk.css"
+  },
+  {
+    "revision": "89c691e12c6acc49eff3",
+    "url": "/My-Spotify/static/js/2.1269ae8e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/My-Spotify/static/js/2.1269ae8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba25a87cd8be612afc7",
+    "url": "/My-Spotify/static/js/main.7876c877.chunk.js"
+  },
+  {
+    "revision": "ecc96666b232bd21c62e",
+    "url": "/My-Spotify/static/js/runtime-main.6a25bc2a.js"
+  }
+]);
